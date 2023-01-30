84aa92406b5d0a2d5b127c2bbac37936c4babd55
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2023-01-30 15:26:57 +0900

    Polish the pages

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Brian";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Blog App";
 
 export default function Layout({ children, home }) {
   return (
